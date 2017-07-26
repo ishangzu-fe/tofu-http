d8ade35e20502bbc6cc583195bf5a71d25ee0fce
hujin0327 <hujin0327@126.com>
2017-07-26 11:46:49 +0800

    fixed object assign

--- src/util.js
+++ src/util.js
@@ -18,8 +18,32 @@ const toJSON = () => {
     return this.Format('yyyy-MM-dd hh:mm:ss');
 }
 
+if (typeof Object.assign != 'function') {
+    (function () {
+        Object.assign = function (target) {
+            'use strict';
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var output = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source !== undefined && source !== null) {
+                    for (var nextKey in source) {
+                        if (source.hasOwnProperty(nextKey)) {
+                            output[nextKey] = source[nextKey];
+                        }
+                    }
+                }
+            }
+            return output;
+        };
+    })();
+}
+
 export default {
-    DateInt:() => {
+    DateInt: () => {
         Date.prototype.Format = Format;
         Date.prototype.toJSON = toJSON;
     }
