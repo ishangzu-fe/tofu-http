28260a026acc490f576e5e2c054530a2e5a12fce
hujin0327 <hujin0327@126.com>
2017-07-26 13:36:28 +0800

    update

--- src/http.js
+++ src/http.js
@@ -6,7 +6,28 @@ class Http {
         return data
     }, headers) {
         util.DateInt();
+        if (typeof Object['assign'] != 'function') {
+            Object.assign = function (target) {
+                'use strict';
+                if (target === undefined || target === null) {
+                    throw new TypeError('Cannot convert undefined or null to object');
+                }
 
+                var output = Object(target);
+                for (var index = 1; index < arguments.length; index++) {
+                    var source = arguments[index];
+                    if (source !== undefined && source !== null) {
+                        for (var nextKey in source) {
+                            if (source.hasOwnProperty(nextKey)) {
+                                output[nextKey] = source[nextKey];
+                            }
+                        }
+                    }
+                }
+                return output;
+            }
+
+        }
         this.isMock = isMock;
         this.chain = chain;
         this.headers = util.merge({
